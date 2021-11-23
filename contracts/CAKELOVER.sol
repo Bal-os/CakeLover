b508f70470444df429e30e87427564c6169d9d11
Bal-os <sbal.stels@gmail.com>
2021-11-23 22:41:18 +0200

    update testnet addresses

--- contracts/CAKELOVER.sol
+++ contracts/CAKELOVER.sol
@@ -1375,7 +1375,7 @@ contract BOXERLOVER is ERC20, Ownable {
 
     	dividendTracker = new BOXERLOVERDividendTracker(address(this));
 
-    	IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1);
+    	IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3);
          // Create a uniswap pair for this new token
         address _uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
